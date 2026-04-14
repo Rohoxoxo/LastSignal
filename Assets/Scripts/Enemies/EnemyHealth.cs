e9100c0ab6dd27c5bbb826f0efe45a95591cb9c6
Rohit Chandel <rohoxoxo@gmail.com>
2026-04-14 14:33:16 -0500

    Updated Speeds of Enemies

--- Assets/Scripts/Enemies/EnemyHealth.cs
+++ Assets/Scripts/Enemies/EnemyHealth.cs
@@ -4,17 +4,20 @@ public class EnemyHealth : MonoBehaviour
 {
     public int maxHealth = 3;
     private int currentHealth;
+    private bool isDead;
 
     void Awake() => currentHealth = maxHealth;
 
     public void TakeDamage(int amount)
     {
+        if (isDead) return;
         currentHealth -= amount;
         if (currentHealth <= 0) Die();
     }
 
     void Die()
     {
+        isDead = true;
         PowerUpSpawner.Instance?.TrySpawnPowerUp(transform.position);
         WaveManager.Instance?.OnEnemyKilled();
         GameManager.Instance?.AddScore(10);
